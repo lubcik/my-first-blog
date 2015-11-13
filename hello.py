789514bf0ddeb750360544ee7086a41ffcd0e7fd
Lubka Kramarekova <lubka.kramarekova@gmail.com>
2015-11-13 12:23:58 +0100

    My Django Girls app, first commit

---
+++ hello.py
@@ -0,0 +1,9 @@
+print('Hello, Django girls!')
+
+if 3 > 2:
+    print('It works!')
+    
+def hi(name):
+    print('Hi ' + name + '!')
+
+hi("Lub")
---
+++ myenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/lubcik/Documents/DjangoGirls/myenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
